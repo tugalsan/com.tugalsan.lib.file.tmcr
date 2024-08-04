1f3cb041b5b78034946601776b4a61979996111b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-08-04 09:01:56 +0300

    Update TS_FileTmcrCodeImageCompile.java

--- src/main/java/com/tugalsan/lib/file/tmcr/server/code/image/TS_FileTmcrCodeImageCompile.java
+++ src/main/java/com/tugalsan/lib/file/tmcr/server/code/image/TS_FileTmcrCodeImageCompile.java
@@ -231,7 +231,7 @@ public class TS_FileTmcrCodeImageCompile {
             d.ci(result.value0, "fromUrl", "ref.init", ref);
             var u_file = TS_PathUtils.toPath(ref);
             if (u_file.isExcuse()) {
-                d.ce("convertLocalLocationToRemote", "is it really path-able?", ref, u_file.excuse().getMessage());
+                d.ce(result.value0, "warning", "convertLocalLocationToRemote#1", "is it really path-able?", ref, u_file.excuse().getMessage());
                 preImageLoc = null;
             } else {
                 preImageLoc = u_file.value();
@@ -258,7 +258,7 @@ public class TS_FileTmcrCodeImageCompile {
 
             u_file = TS_PathUtils.toPath(ref);
             if (u_file.isExcuse()) {
-                d.ce("convertLocalLocationToRemote", "is it really path-able?", ref, u_file.excuse().getMessage());
+                d.ce(result.value0, "warning", "convertLocalLocationToRemote#2", "is it really path-able?", ref, u_file.excuse().getMessage());
                 preImageLoc = null;
             } else {
                 preImageLoc = u_file.value();
