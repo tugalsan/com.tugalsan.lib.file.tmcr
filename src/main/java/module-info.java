e545e95ed5369e1e75adc015971a52b59fb633f5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-08-07 16:20:28 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,7 +15,6 @@ module com.tugalsan.lib.file.tmcr {
     requires com.tugalsan.api.file.pdf;
     requires com.tugalsan.api.file.img;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.lib.acsrf;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.charset;
     requires com.tugalsan.api.thread;
